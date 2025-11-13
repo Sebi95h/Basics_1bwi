e7951710a4f3c5bec25534224e27acd1a259fe15
Sebastian <you@example.com>
2025-11-13 11:48:36 +0100

    opening apple/banana

---
+++ 30_Aufgabe zu Variablen/01_Aufgabe.js
@@ -0,0 +1,11 @@
+let bananaWeight =  0.22;
+let appleWeight = 0.34;
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+
+let weightOf4Bananas = bananaWeight*4;
+let priceOf4Bananas = weightOf4Bananas * bananaPricePerKilo;
+console.log (applePricePerKilo);
+ 
