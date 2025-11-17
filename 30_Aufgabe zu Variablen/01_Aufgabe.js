2ed0388d86290df854dcafaa0b3f656c513f37e7
Sebastian <you@example.com>
2025-11-17 11:06:47 +0100

    Understand 30_Aufgabe zu Variablen

---
+++ 30_Aufgabe zu Variablen/01_Aufgabe.js
@@ -0,0 +1,15 @@
+let bananaWeight =  0.22;
+let appleWeight = 0.34;
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+
+let weightOf4Bananas = bananaWeight*4;
+let priceOf4Bananas = weightOf4Bananas * bananaPricePerKilo;
+//console.log (applePricePerKilo);
+
+console.log("Apple price per Kilo:", applePricePerKilo);
+console.log("Banana price per Kilo:", bananaPricePerKilo);
+
+console.log("Price for 8 apple:",8 * applePricePerKilo);
