4b4fd9762d169831d4c6f8a04f22565eda44ce1f
Sebastian <you@example.com>
2025-11-20 12:59:20 +0100

    finished first task of 42

---
+++ 42_if Bedinungen if_else/01_Aufgabe if else.js
@@ -0,0 +1,19 @@
+let a = Math.floor(Math.random() * 10)
+
+console.log(a);
+
+
+if (a < 5) {
+    console.log("The number is less than 5");
+    
+} else if (a > 5) {
+    console.log("The number is greater than 5")
+    
+} else if (a == 5) {
+    console.log("The number is exactly 5");
+    
+}
+    
+
+   
+    
