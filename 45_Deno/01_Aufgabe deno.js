877d3b0313c4868ceb076948ea63ab97a87d7459
Sebastian <you@example.com>
2025-12-04 12:14:49 +0100

    finished 01_Aufgabe deno

---
+++ 45_Deno/01_Aufgabe deno.js
@@ -0,0 +1,13 @@
+let input = prompt("Gib dein Passwort ein")
+
+console.log(input);
+
+if (input === "gooner") {
+    console.log("Zugriff Gewährt");
+
+} else {
+
+    console.log("Zugriff Verweigert");
+
+}
+
