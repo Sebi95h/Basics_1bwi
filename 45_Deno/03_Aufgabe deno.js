99f0bf74f88155471384e09a1e94a4a7179e986f
Sebastian <you@example.com>
2025-12-04 13:05:05 +0100

    finished 03_Aufgabe deno

--- 45_Deno/03_Aufgabe deno.js
+++ 45_Deno/03_Aufgabe deno.js
@@ -1,1 +1,26 @@
-prompt()
+let Number1 = Number(prompt("Gib deine erste Zahl ein"));
+let Number2 = Number(prompt("Gib deine zweite Zahl ein"));
+let type = prompt("Gib die Rechenart ein");
+
+
+if (type == "+") {
+    let result = Number1 + Number2
+
+    console.log(result)
+
+} if (type == "-") {
+    let result = Number1 - Number2
+
+    console.log(result)
+
+} if (type == "*") {
+    let result = Number1 * Number2
+
+    console.log(result)
+
+} if (type == "/") {
+    let result = Number1 / Number2
+
+    console.log(result)
+}
+
