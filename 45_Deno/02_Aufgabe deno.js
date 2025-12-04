6d6050e3d6830b8a5e1692a1918ef4661a708549
Sebastian <you@example.com>
2025-12-04 12:33:26 +0100

    finished task 02&03 deno

---
+++ 45_Deno/02_Aufgabe deno.js
@@ -0,0 +1,14 @@
+let number = -4;
+
+if (number > 0) {
+    console.log("Die Nummer ist Positiv");
+
+} else if (number < 0) {
+
+    console.log("Die Nummer ist Negativ");
+
+} else {
+    number === 0
+    console.log("Die Nummer ist Null");
+
+}
