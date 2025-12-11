25a2201f5764b272e915391da0379a3cfbd9b651
Sebastian <you@example.com>
2025-12-11 12:17:08 +0100

    finished 04_Aufgabe deno

---
+++ 45_Deno/04_Aufgabe deno.js
@@ -0,0 +1,15 @@
+let Alter = (prompt("Gib dein Alter ein"))
+
+if (Alter < 25) {
+    console.log("jung");
+
+} else if (Alter > 25 && Alter < 50) {
+    console.log("mittelalt");
+
+} else if (Alter > 50)
+    console.log("alt");
+
+
+
+
+
