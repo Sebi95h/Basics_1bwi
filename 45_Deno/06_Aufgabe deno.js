9d9fa321a4cc7bc91ebf4fd45e0fdf6a3e623656
Sebastian <you@example.com>
2025-12-18 11:37:02 +0100

    finished 06_Aufgabe_deno

--- 45_Deno/06_Aufgabe deno.js
+++ 45_Deno/06_Aufgabe deno.js
@@ -1,2 +1,20 @@
-console.log(prompt("Welches Getränkt willst du haben? Colo, Wasser, Bier oder Tee"));
-
+let Getränkt = (prompt("Welches Getränkt willst du haben? Colo, Wasser, Bier oder Tee ="));
+let Cola = 1
+let Wasser = 2
+let Bier = 3
+let Tee = 4
+if (Getränkt == "Cola") {
+    console.log("Du hast Cola gewählt");
+}
+else if (Getränkt == "Wasser") {
+    console.log("Du hast Wasser gewählt");
+}
+else if (Getränkt == "Bier") {
+    console.log("Du hast Bier gewählt");
+}
+else if (Getränkt == "Tee") {
+    console.log("Du hast Tee gewählt");
+}
+else {
+    console.log("Das Getränkt gibt es nicht");
+}
