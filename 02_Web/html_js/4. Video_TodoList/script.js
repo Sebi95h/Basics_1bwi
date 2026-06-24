7796d1d52df0baf4beb36308198106fe6aa4f7ea
Sebastian <you@example.com>
2026-06-24 09:32:19 +0200

    Todoliste

---
+++ 02_Web/html_js/4. Video_TodoList/script.js
@@ -0,0 +1,12 @@
+let todos = [
+    {id:1, title:"putzen", isDone:false},
+    {id:2, title:"kochen", isDone:true},
+    {id:3, title:"einkaufen", isDone:false},
+    {id:4, title:"lernen", isDone:false},
+]
+
+
+
+todos.forEach(element => {
+    console.log(element.title);
+});
